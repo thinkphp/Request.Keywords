00d57f292e9031934041c6aa98f2ad96c198a28e
Adrian Statescu <mergesortv@gmail.com>
2010-07-28 17:07:48 +0300

    updated source and docs

--- Source/Request.Keywords.js
+++ Source/Request.Keywords.js
@@ -1,3 +1,20 @@
+/*
+---
+description: This is a quick hack based on MooTools which tries to find keywords connected to a certain term.
+
+authors:
+- Adrian Statescu (http://thinkphp.ro)
+
+license:
+- MIT-style license
+
+requires:
+ core/1.2.1: '*'
+
+provides: [Request.Keywords]
+...
+*/
+
 Request.Keywords = new Class({
         Extends: Request.JSONP,
         options: {
@@ -7,8 +24,36 @@ Request.Keywords = new Class({
               diagnostics: true 
            } 
         },
-        initialize: function(term,language,region,options){
+        initialize: function(term,reg,lan,options){
+                       if(typeof reg == 'string' && reg) {region = reg;}else{region = 'us';}  
+                       if(typeof lan == 'string' && lan) {language = lan;}else{language = 'en';} 
                        this.options.url = this.options.url.substitute({term: term,language: language, region: region});
                        this.parent(options);
         }  
 }); 
+
+Element.implement({
+      loadKeywords: function(term,reg,lan) {         
+           var elem = this;
+               if(!reg) {reg = 'us';}
+               if(!lan) {lan = 'en';}
+                new Request.Keywords(term, reg, lan, {
+                          onSuccess: function(o) {
+                                     if(window.console) {console.log(o);}  
+                                     if(o.query.results.keywords) {
+                                             var kw = o.query.results.keywords.terms;
+                                             var out = '<p class="termslist">' + kw.replace(/,/g,', ') + '</p>';
+                                             elem.set('html',out);
+                                      } else {
+                                             elem.set('text','There was an error, please try again.');
+                                      }
+                          },
+                          onRequest: function(script) {
+                               if(window.console) {console.log(script);}  
+                               elem.set('text','Loading...');
+                          } 
+
+               }).send();
+       return this;
+      }
+});
